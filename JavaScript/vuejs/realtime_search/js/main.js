269123f3aa76faead02e20b3a9ceba6f14acf5af
ryuckel <36391432+ryuckel@users.noreply.github.com>
2019-12-31 12:36:02 +0900

    Add files via upload

---
+++ JavaScript/vuejs/realtime_search/js/main.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: null,
+        keyword: "",
+        message: "",
+    },
+    watch: {
+        keyword: function(newKeyword, oldKeyword){
+            // console.log(newKeyword)
+this.message = "Waiting for you stop writing"
+this.debouncedGetAnswer()
+        }
+    },
+    created: function() {
+        // this.keyword = "JavaScript"
+        // this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+
+    },
+    methods: {
+        getAnswer: function(){
+            if(this.keyword === ""){
+                this.items = null
+                this.message = ""
+                return
+            }
+
+            this.message = "Loading..."
+            var vm = this
+            var params = { page:1, per_page: 20, query: this.keyword }
+            axios.get('https://qiita.com/api/v2/items', {params})
+            .then(function(responce){
+                console.log(responce);
+                vm.items = responce.data
+            })
+            .catch(function(error){
+                vm.message = "Error"+ error
+            })
+            .finally(function(){
+                vm.message = ""
+            })
+        }
+    }
+})
